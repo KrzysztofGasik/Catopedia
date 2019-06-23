0ae4736d7fdded206dad63fbe7c7c50dbaee580b
KrzGas <kgasik@gmail.com>
2019-06-23 19:58:52 +0200

    looks quite good

---
+++ src/Components/CatImage/CatImage.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class CatImage extends Component {
+  state = {
+    cat: "",
+    error: false
+  };
+
+  // componentDidMount() {
+  //   this.getNextCat();
+  // }
+
+  getNextCat = () => {
+    fetch("https://aws.random.cat/meow")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          cat: data
+        });
+      })
+      .catch(() => {
+        this.setState({
+          error: true
+        });
+      });
+  };
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div>
+          <h1>Błąd konmunikacji z serwerem</h1>
+          <button>Spróbuj ponownie</button>
+        </div>
+      );
+    }
+    return (
+      <>
+        <button className="random__cat" onClick={this.getNextCat}>Get random cat image</button>
+        <img src={this.state.cat.file}/>
+      </>
+    );
+  }
+}
+
+export {CatImage};
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Home = () => {
+    return (
+        <h2>Hey there, welcome to Catopedia, app about cats - meow you</h2>
+    )
+}
+
+export {Home};
